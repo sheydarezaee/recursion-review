20e137561300064bb36b5ccd8d31e11067fdab19
Hack Reactor Student <student@hackreactor.com>
2017-03-27 16:29:32 -0700

    Min requirment complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+  var result = [];
+  if (document.body.classList.contains(className)) {
+    result.push(document.body);
+  }
+  help(className, document.body.childNodes, result);
+  return result;
+}
+
+var help = function(className, childNodes, result) {
+  if (childNodes.length === 0) {
+  	return;
+  }
+  else {
+    for (var i = 0; i < childNodes.length; i++) {
+      var thisElement = childNodes.item(i);
+      if ((thisElement !== undefined && thisElement.classList !== undefined)
+      	&& (thisElement.classList.contains(className))) {
+      	result.push(thisElement);
+      }
+      help(className, thisElement.childNodes, result);
+    }
+    return;
+  }
+}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+var stringifyJSON = function(obj)  {
+  if (typeof obj === 'undefined' || typeof obj === 'function') {
+  	//do nothing
+  	return undefined;
+  }
+  else if (typeof obj === 'string') {
+  	return '\"' + obj + '\"';
+  }
+  else if (typeof obj === 'boolean') {
+  	return '' + obj;
+  }
+  else if (typeof obj !== 'object') {
+  	//if obj is not a object
+  	return '' + obj;
+  }
+  else if (obj === null) {
+  	//if obj is null
+  	return 'null';
+  }
+  else if (Array.isArray(obj)){
+  	//if obj is array
+  	var content = '[';
+  	for (var i = 0; i < obj.length; i++) {
+  		content += stringifyJSON(obj[i]);
+  		if (i != obj.length - 1) {
+  			content += ',';
+  		}
+  	}
+  	content = content + ']';
+  	return content;
+  }
+  else {
+  	//if obj is object and null
+  	var content = '{';
+  	var hasObj = false;
+  	for (var key in obj) {
+  		
+  		var value = stringifyJSON(obj[key]);
+  		if (typeof value !== 'undefined') {
+  			hasObj = true;
+  			content += '"' + key + '":' + value + ',';
+  		}
+  	}
+  	//if obj is not empty, then delete the last ,
+  	if (hasObj) {
+  		content = content.slice(0,-1);
+  	}
+  	content = content + '}';
+  	return content;
+  }
+}
